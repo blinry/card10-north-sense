5a952daa21ff90439c3ca203523d498501f20a88
Sebastian Morr <sebastian@morr.cc>
2019-09-10 19:18:21 +0200

    Auto formatting with black

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ import display
 import vibra
 import buttons
 
-fov = 45 # field of vision in degrees
-freq = 0.2 # second
+fov = 45  # field of vision in degrees
+freq = 0.2  # second
 
 orientation = bhi160.BHI160Orientation()
 disp = display.open()
@@ -27,31 +27,27 @@ while True:
             color = [0, 200, 0]
 
         disp.clear()
-        disp.print("%f" % degrees, posy = 20, fg = color)
-        disp.print("%f" % freq, posy = 40)
+        disp.print("%f" % degrees, posy=20, fg=color)
+        disp.print("%f" % freq, posy=40)
         disp.update()
 
         if sample.status == 3:
-            pointing_north = degrees > 360-fov/2 or degrees < fov/2
+            pointing_north = degrees > 360 - fov / 2 or degrees < fov / 2
             vibra.set(pointing_north)
-            utime.sleep(freq/2)
+            utime.sleep(freq / 2)
             vibra.set(False)
-            utime.sleep(freq/2)
+            utime.sleep(freq / 2)
         else:
             utime.sleep(freq)
     else:
         utime.sleep(freq)
 
     # check for button presses
-    v = buttons.read(
-        buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT | buttons.TOP_RIGHT
-    )
+    v = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT | buttons.TOP_RIGHT)
     button_pressed = v != 0
     if button_pressed and v & buttons.BOTTOM_LEFT != 0:
         freq -= 0.05
-        pass
     elif button_pressed and v & buttons.BOTTOM_RIGHT != 0:
         freq += 0.05
-        pass
     elif button_pressed and v & buttons.TOP_RIGHT != 0:
         pass
